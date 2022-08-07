8c161863faaf3dadc4d73d30e2ebc14e5e0898c4
Tia44GG <95694773+Zylux06@users.noreply.github.com>
2022-08-07 19:31:24 +0200

    ciao

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -3,26 +3,22 @@ const Discord = require("discord.js")
 module.exports = {
     name: "invite",
     description: "Invia il link d invito del bot",
-    execute(message, args) {
+    execute(message) {
+        let embed = new Discord.MessageEmbed()    
+            .setTitle("__**Link Invito Bot**__")
+            .setColor("RANDOM")
+            .setDescription(`*${message.author.toString()} Clicca il pulsante qui sotto per invitare il Bot!*`)
+            .setFooter({text: "Invito al Bot!"})
+            .setTimestamp()
 
-        let row = new Discord.MessageActionRow()
-
-        .addComponents(
-            new MessageButton()
-                .setCustomId('IdBut1')
-                .setLabel('Invito Bot')
-                .setStyle('LINK')
-                .setEmoji('📩')
-                .setURL("https://discord.com/api/oauth2/authorize?client_id=1004779665749188629&permissions=8&scope=bot")
-        )               
+        message.channel.send({ embeds: [embed], components: [row], ephemeral: true })
         
-            var embed = new Discord.MessageEmbed()    
-                .setTitle("__**Link Invito Bot**__")
-                .setColor("RANDOM")
-                .setDescription(`*${message.author.toString()} Clicca il pulsante qui sotto per invitare il Bot!*`)
-                .setFooter({text: "Invito al Bot!"})
-                .setTimestamp()
-
-            message.channel.send({ embeds: [embed], components: [row], ephemeral: true })
-
-        }}
+        let button = new Discord.MessageButton()
+            .setCustomId('IdBut1')
+            .setLabel('Invito Bot')
+            .setStyle('LINK')
+            .setEmoji(':envelope_with_arrow:')
+            .setURL("https://discord.com/api/oauth2/authorize?client_id=1004779665749188629&permissions=8&scope=bot")
+        let row = new Discord.MessageActionRow().addComponents(button)
+    }
+}
