66386d459d1bb06bf7ffa3f89ad93b0f3a6df00d
Tia44GG <95694773+Zylux06@users.noreply.github.com>
2022-08-07 19:36:43 +0200

    add

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -11,14 +11,14 @@ module.exports = {
             .setFooter({text: "Invito al Bot!"})
             .setTimestamp()
 
-        message.channel.send({ embeds: [embed], components: [row], ephemeral: true })
-        
         let button = new Discord.MessageButton()
             .setCustomId('IdBut1')
             .setLabel('Invito Bot')
             .setStyle('LINK')
-            .setEmoji('📩')
+            .setEmoji(':envelope_with_arrow:')
             .setURL("https://discord.com/api/oauth2/authorize?client_id=1004779665749188629&permissions=8&scope=bot")
-        let row = new Discord.MessageActionRow().addComponents(button)
+        let row = new Discord.MessageActionRow()
+            .addComponents(button)
+        message.channel.send({ embeds: [embed], components: [row], ephemeral: true })
     }
 }
