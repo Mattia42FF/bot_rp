5ac1704dd494cf2ab6eb992503c845eb528c2f56
Tia44GG <95694773+Zylux06@users.noreply.github.com>
2022-08-10 19:33:17 +0200

    ciao1

---
+++ commands/general/comento.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "commento",
+    description: "Per commentare un Posto",
+    execute(message, args) {
+        
+        var testo;
+        testo = args.join(" ");
+        if (!testo) {
+            return message.channel.send("Inserire un Messaggio");
+        }
+
+        message.delete()
+
+        var embed = new Discord.MessageEmbed()
+            .setTitle("__**Nuovo Commento!**__")
+            .setColor("RANDOM")
+            .setImage("https://cdn.discordapp.com/attachments/1005194612102672446/1006978320048590888/unknown.png")
+            .setDescription(`**${message.author.toStrig()} Ha Appena Commentato al Post: ${testo}.**`)
+            .setFooter({ text: "Nuovo Commento" })
+            .setTimestamp()
+
+            message.channel.send({ embeds: [embed] })
+
+    }
+}                
