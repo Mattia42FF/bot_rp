a076f6ef364dd798c6426e80d8b1b974ea22b683
Tia44GG <95694773+Zylux06@users.noreply.github.com>
2022-09-02 11:15:07 +0200

    fs

--- commands/staff/passato.js
+++ commands/staff/passato.js
@@ -22,8 +22,8 @@ module.exports = {
         var embed = new Discord.MessageEmbed()
             .setTitle("<:Accettato:1007556821625622668> __**Whitelist Passata**__ <:Accettato:1007556821625622668>")
             .setColor("RANDOM")
+            .addField('Whitelist', 'Passata', false)
             .addField('Whitelist Eseguita da:',  `*${message.author.toString()}*`, false)
-            .addField('Motivo', `*${testo}*`, false)
             .setFooter({text: "Italian Rome Full RP" })
             .setTimestamp()
 
