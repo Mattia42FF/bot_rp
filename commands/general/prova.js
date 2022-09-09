5af31aef6df0ddc013d3e88cf1b6c4f7b85b9714
Tia44GG <95694773+Zylux06@users.noreply.github.com>
2022-09-09 09:35:28 +0200

    fs

---
+++ commands/general/prova.js
@@ -0,0 +1,15 @@
+const { DiscordAPIError } = require("discord.js")
+
+module.exports = {
+    name: "prova",
+    description: "prova comando",
+    data: {
+        name: "prova",
+        description: "prova di comando"
+    },
+    execute(interaction) {
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Prova")
+        interaction.reply({ embeds: [embed], ephemeral: true })
+    }
+}
